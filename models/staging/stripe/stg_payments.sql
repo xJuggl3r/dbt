e4eaa1630bb001aad808e0ab820b0764248c6911
xjuggl3r <xjuggl3r@gmail.com>
2023-03-17 19:51:44 -0400

    macroing

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -4,6 +4,7 @@ select
     paymentmethod as payment_method,
     status,
     -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount,
+    {{ cents_to_dollars('amount', 2)}},
     created as created_at
 from {{ source('stripe', 'payment') }}
+
