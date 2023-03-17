1dacdec4b021bb3ad5a8bc7b1a755139aeb7adee
xjuggl3r@gmail.com <xjuggl3r@gmail.com>
2023-03-17 22:23:18 +0000

    fixed typo

--- models/staging/jaffle_shop/marts/core/int_orders__pivoted.sql
+++ models/staging/jaffle_shop/marts/core/int_orders__pivoted.sql
@@ -1,4 +1,4 @@
-{% set payments_methods = ['bank_transfer', 'coupon', 'credit_card', 'gift_card']%}
+{%- set payments_methods = ['bank_transfer', 'coupon', 'credit_card', 'gift_card'] -%}
 
 with payments as (
     select * from {{ ref('stg_payments')}}
@@ -6,12 +6,12 @@ with payments as (
 
 pivoted as (
     select order_id,
-    {% for payment_method in payments_methods %}
+    {% for payment_method in payments_methods -%}
     sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount
-    {% if not loop.last %}
+    {%- if not loop.last -%}
     ,
-    {% endif %}
-    {% endfor %}
+    {% endif -%}
+    {%- endfor %}
 
 from payments
 where status = 'success'
