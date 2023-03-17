4e9e0aeab6c30e05c3323ed93e7ea86d2888dcdb
xjuggl3r@gmail.com <xjuggl3r@gmail.com>
2023-03-17 22:07:58 +0000

    seeds

--- analyses/total_revenue.sql
+++ analyses/total_revenue.sql
@@ -2,7 +2,7 @@
 total_revenue.sql that uses the stg_payments model and sums the amount of successful payments. 
 (Remember to use Jinja in this rather than the raw table name) #}
 
-with total_revenue as (
+with payments as (
   select * from {{ ref('stg_payments') }}
 )
 
@@ -10,5 +10,5 @@ select sum(amount) as total_revenue
 
 from total_revenue
 
-where status = 'successful'
+where status = 'success'
 
