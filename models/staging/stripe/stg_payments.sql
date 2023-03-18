5592e97e3317729be44e79fdd7635e4f1d19712e
xjuggl3r <xjuggl3r@gmail.com>
2023-03-17 20:30:40 -0400

    fix typo

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -3,7 +3,7 @@ select
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-    -- amount is stored in cents, convert it to dollars
+    -- amount is stored in cents, convert it to dollars 
     {{ cents_to_dollars('amount', 2)}},
     created as created_at
 from {{ source('stripe', 'payment') }}
